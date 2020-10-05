a7c354476ddfe1fbb336df95dfe8a02f2d5a195c
KwanjiChoi <kwanji0912@gmail.com>
2020-10-06 01:38:38 +0900

    run rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,7 +12,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.use_transactional_fixtures = true
@@ -20,5 +19,4 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.filter_rails_from_backtrace!
-
 end
